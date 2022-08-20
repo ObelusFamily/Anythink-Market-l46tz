1e7a7af83aec1db8b0c3a5ceaaf3ba2f0df866ad
Akshay Rajpurohit <akj.rajpurohit@gmail.com>
2022-08-20 23:04:23 +0530

    Add placeholder and onError for img tag in ItemPreview

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import placeholderImg from "../imgs/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -37,6 +38,8 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         src={item.image}
+        placeholder={placeholderImg}
+        onError={placeholderImg}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
